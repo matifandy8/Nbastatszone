dcbf820dfd76a52bebbff8d0a1848b3fae29aff5
matias <matifandy@gmail.com>
2024-04-27 16:57:50 -0300

    feat: add logo to game info

--- src/app/ui/layout/livegames/game.tsx
+++ src/app/ui/layout/livegames/game.tsx
@@ -1,22 +1,38 @@
 import styles from "./liveGames.module.css";
 import Image from "next/image";
 
-export default function Game({ data }: any) {
+export default async function Game({ data }: any) {
   return (
     <div className={styles.game}>
       <div className={styles.time}>
-        <span className={styles.timeText}>{data.time}</span>
+        <span className={styles.timeText}>{data.time || "Today"}</span>
       </div>
       <div className={styles.teams}>
         <div className={styles.teamContainer}>
-          <Image src={data.image} alt="" width={20} height={20} />
+          <Image
+            src={
+              "../../images/logos-nba/logo-" + data.team.toLowerCase() + ".svg"
+            }
+            alt={data.team}
+            width={20}
+            height={20}
+          />
           <div className={styles.team}>
             <span className={styles.teamName}>{data.team}</span>
             <span className={styles.teamScore}>{data.score}</span>
           </div>
         </div>
         <div className={styles.teamContainer}>
-          <Image src={data.imagetwo} alt="" width={20} height={20} />
+          <Image
+            src={
+              "../../images/logos-nba/logo-" +
+              data.teamtwo.toLowerCase() +
+              ".svg"
+            }
+            alt={data.teamtwo + " logo"}
+            width={20}
+            height={20}
+          />
           <div className={styles.team}>
             <span className={styles.teamName}>{data.teamtwo}</span>
             <span className={styles.teamScore}>{data.scoretwo}</span>
